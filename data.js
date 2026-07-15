9423c8e73ff1cdaeb442794ca05baad900df3d4f
cartoon1223330-ux <cartoon1223330@gmail.com>
2026-07-15 15:45:09 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,45 +1,44 @@
 const products = [
-
-{
-id:1,
-name:"Laptop",
-price:59999,
-image:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=500"
-},
-
-{
-id:2,
-name:"Smartphone",
-price:24999,
-image:"https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=500"
-},
-
-{
-id:3,
-name:"Headphones",
-price:3999,
-image:"https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500"
-},
-
-{
-id:4,
-name:"Smart Watch",
-price:7999,
-image:"https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500"
-},
-
-{
-id:5,
-name:"Camera",
-price:45999,
-image:"https://images.unsplash.com/photo-1516035069371-29a1b244cc32?w=500"
-},
-
-{
-id:6,
-name:"Keyboard",
-price:1999,
-image:"https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?w=500"
-}
-
+  {
+    id: 1,
+    name: "Laptop",
+    category: "Laptop",
+    price: 59999,
+    image: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?w=500"
+  },
+  {
+    id: 2,
+    name: "Smartphone",
+    category: "Mobile",
+    price: 24999,
+    image: "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?w=500"
+  },
+  {
+    id: 3,
+    name: "Headphones",
+    category: "Audio",
+    price: 3999,
+    image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?w=500"
+  },
+  {
+    id: 4,
+    name: "Smart Watch",
+    category: "Watch",
+    price: 7999,
+    image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=500"
+  },
+  {
+    id: 5,
+    name: "Camera",
+    category: "Camera",
+    price: 45999,
+    image: "https://images.unsplash.com/photo-1516035069371-29a1b244cc32?w=500"
+  },
+  {
+    id: 6,
+    name: "Keyboard",
+    category: "Accessories",
+    price: 1999,
+    image: "https://images.unsplash.com/photo-1511467687858-23d96c32e4ae?w=500"
+  }
 ];
